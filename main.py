84cb81ba0766451f868838bf4b5a703ca253d8d4
rvouilloz <97536689+rvouilloz@users.noreply.github.com>
2024-10-07 14:39:33 +0200

    init

---
+++ main.py
@@ -0,0 +1,67 @@
+#import FreeCAD libraries
+import FreeCAD as App
+import Part, math
+
+#import Speckle libraries
+from typing import List
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_default_account
+from specklepy.objects import Base
+from specklepy.objects.geometry import (
+     Mesh, Curve, Interval, Box, Point, Plane, Vector, Polyline,
+)
+
+#set links to Speckle
+client = SpeckleClient(host="https://app.speckle.systems/")
+account = get_default_account()
+client.authenticate_with_account(account)
+stream_id = "6cf358a40e"
+
+#set container of all elements
+data = Base()
+data.elements = []
+
+#get FreeCAD Part objects
+objects = App.ActiveDocument.Objects
+for object in objects:
+	if object.TypeId == 'Part::Box':
+		#extract FreeCAD properties
+		propertiesList = object.PropertiesList
+		height = object.Height
+		length = object.Length
+		placement = object.Placement
+		shape = object.Shape
+		width = object.Width
+		#extract FreeCAD placement properties
+		angle = placement.Rotation.Angle
+		axis = placement.Rotation.Axis		
+		position = placement.Base
+		#create Speckle basePlane
+		basePlane = Plane.from_list([
+			position.x, position.y, position.z,
+			axis.x, axis.y, axis.z,
+			math.cos(angle), math.sin(angle), 0.0,
+			math.cos(angle+math.pi/2), math.sin(angle+math.pi/2), 0.0,
+		])
+		print(basePlane.to_list())
+		#create Speckle box
+		myBox = Box(
+		    basePlane = basePlane,
+		    xSize = Interval.from_list([0, width.Value]),
+		    ySize = Interval.from_list([0, length.Value]),
+		    zSize = Interval.from_list([0, height.Value])
+		)
+		#add to data to be sent
+		data.elements.append(myBox)
+
+#send to Speckle
+from specklepy.transports.server import ServerTransport
+from specklepy.api import operations
+transport = ServerTransport(client=client, stream_id=stream_id)
+hash = operations.send(base=data, transports=[transport])
+commid_id = client.commit.create(
+    stream_id=stream_id, 
+    object_id=hash, 
+    message="this is Parts I made in FreeCAD",
+    )
+print("sent")
